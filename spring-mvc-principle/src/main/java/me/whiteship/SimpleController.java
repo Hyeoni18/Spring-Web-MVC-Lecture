8f5387cbd55e6aa72986d6194cbc60a36a11d806
Hyeoni18 <gusgnl105@gmail.com>
2022-02-02 12:31:29 +0900

    DispatcherServlet 3부

--- spring-mvc-principle/src/main/java/me/whiteship/SimpleController.java
+++ spring-mvc-principle/src/main/java/me/whiteship/SimpleController.java
@@ -11,6 +11,6 @@ public class SimpleController implements Controller {
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return new ModelAndView("simple");
+        return new ModelAndView("simple"); //원래는 /WEB-INF/simple.jsp 이지만 WebConfig.java 설정으로 인해 simple 만 적어줘도 됨.
     }
 }
--- spring-mvc-principle/src/main/java/me/whiteship/WebConfig.java
+++ spring-mvc-principle/src/main/java/me/whiteship/WebConfig.java
@@ -1,20 +1,27 @@
 package me.whiteship;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+@ComponentScan
+public class WebConfig {
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/", ".jsp");
+    //뷰리졸버를 등록해볼거야.
+    @Bean
+    public ViewResolver viewResolver() {
+        //우리가 기본으로 사용하게 되는 리졸버가 맞아.
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        //근데 우리가 설정을 조금 해볼거야.
+        viewResolver.setPrefix("/WEB-INF/"); //항상 여기 밑에 두겠다.
+        viewResolver.setSuffix(".jsp"); //다 jsp 로 끝난다.
+        return viewResolver;
     }
-
 }
