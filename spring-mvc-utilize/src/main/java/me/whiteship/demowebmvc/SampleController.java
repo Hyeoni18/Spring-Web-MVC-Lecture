a24c47e35d8586b11cc1c665cdafaa86d882b4ab
Hyeoni18 <gusgnl105@gmail.com>
2022-02-06 07:51:15 +0900

    요청 매핑하기 2부 URI 패턴

--- spring-mvc-utilize/src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ spring-mvc-utilize/src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -1,19 +1,24 @@
 package me.whiteship.demowebmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(method = RequestMethod.GET)
+@RequestMapping(method = RequestMethod.GET, value = "hello")
 public class SampleController {
 
-    @RequestMapping("/hello")
-//    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.PUT}) //배열로 둘 다 허용한다. 라고 줄 수 있음.
-//    @RequestMapping(value = "/hello", method = RequestMethod.GET) //url 패턴만 주면 모든 HTTP 메소드가 매핑이 됨. (get, post, delete...)
-    @ResponseBody //그냥 응답으로 보내고 싶을 때
+    @RequestMapping("/boa")
+    @ResponseBody
     public String hello() {
-        return "hello";
+        return "hello boa";
     }
+
+//    @RequestMapping("/**")
+//    @ResponseBody
+//    public String helloName() {
+//        return "hello myname";
+//    }
 }
--- spring-mvc-utilize/src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ spring-mvc-utilize/src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -11,8 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // Junit 어노테이션, 스프링에서 제공하는 Junit Runner. 스프링 테스트 실행할 때 효율적으로 실행할 수 있도록 도와줌. 내부적으로 애플리케이션 컨텍스트도 만들어줌. 테스트에서 사용할.
 @WebMvcTest //web만 테스트 mock 을 사용할 수 있게 해줌.
@@ -23,21 +22,12 @@ public class SampleControllerTest extends TestCase {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(post("/hello")) //mockMvc 에서 perform 을 하면 요청을 보내는건데, get으로 요청을 만들어 보낼 수 있음.
-                .andDo(print())  //요청을 만들어 보내면 그 다음 이걸 하고 싶다. 요청과 응답을 출력하고 싶다.
-//                .andExpect(status().isOk()) //어떤 결과를 기대한다.
-                .andExpect(status().isMethodNotAllowed())
-//                .andExpect(content().string("hello")) //본문의 있는 문자열이 hello 라고 나올 것이다.
-        ;
-
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello/boa.jsp"))
                 .andDo(print())
                 .andExpect(status().isOk())
-        ;
-
-        mockMvc.perform(put("/hello"))
-                .andDo(print())
-                .andExpect(status().isMethodNotAllowed())
+                .andExpect(content().string("hello boa"))
+                .andExpect(handler().handlerType(SampleController.class)) //핸들러 타입은 컨트롤러가 처리할 것이고
+                .andExpect(handler().methodName("hello")) //핸들러의 메서드 이름
         ;
     }
 
