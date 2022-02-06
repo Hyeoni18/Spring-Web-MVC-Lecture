159bf6bfeb2200c04aa0af80292a42dc0c590c90
Hyeoni18 <gusgnl105@gmail.com>
2022-02-06 15:56:09 +0900

    핸들러 메소드 6부 @Validated

--- spring-mvc-utilize/src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ spring-mvc-utilize/src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,11 +24,8 @@ public class SampleController {
 
     @PostMapping("/events/name/{name}")
     @ResponseBody
-    public Event hello(@Valid @ModelAttribute Event event, BindingResult bindingResult) {
-        //limit 에 최소 값 0을 설정해두고 - 값이 넘어올 때. 바인딩이 되긴 함.
-        //이후 @Valid 이 실행 되는거임. 그리고 여기서 오류가 나도 bindingResult 에 담김.
-        //그래서 응답이 끝난 후 limit 에 값이 담겨있긴 하지만 error 도 담긴 것을 볼 수 있음
-        //Field error in object 'event' on field 'limit': rejected value [-10]; codes [Min.event.limit,Min.limit,Min.java.lang.Integer,Min]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [event.limit,limit]; arguments []; default message [limit],0]; default message [must be greater than or equal to 0]
+    public Event hello(@Validated(Event.ValidateName.class) @ModelAttribute Event event, BindingResult bindingResult) {
+        //Event 에는 NotBlank 를 보면 그룹으로 지정할 수가 있음. 근데 @Valid 는 그룹을 설정할 수 없음.
         if(bindingResult.hasErrors()) {
             System.out.println("=========================================");
             bindingResult.getAllErrors().forEach( c -> System.out.println(c) );
