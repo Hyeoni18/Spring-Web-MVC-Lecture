f23d26b731561010511d0138f1515efbde8de6a3
Hyeoni18 <gusgnl105@gmail.com>
2022-02-06 09:03:27 +0900

    요청 맵핑하기 5부 HEAD와 OPTIONS

--- spring-mvc-utilize/src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ spring-mvc-utilize/src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -3,26 +3,20 @@ package me.whiteship.demowebmvc;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SampleController {
 
-    // "/hello?name=boa" 요청에 보내는 파라미터들. 도 헤더와 마찬가지로 적용할 수 있음.
-
-    @RequestMapping(
-            value = "/hello"
-            //,headers = HttpHeaders.FROM //문자열을 줄 수 있어. 이 헤더가 들어있는 요청만 처리할거야.
-            //,headers = HttpHeaders.ACCEPT_LANGUAGE //그냥 ACCEPT 는 걸러지지가 않아. 응답 쪽에서 명시적으로 보내지 않더라도 매칭이 됨.
-            //,headers = "!"+HttpHeaders.AUTHORIZATION // 이게 없어여 된다. ! 을 사용할 수 있음.
-            ,headers = HttpHeaders.FROM + "=localhost" //정확히 일치하는 경우
-            ,params = "name=spring"
-    )
+    @GetMapping("hello")
     @ResponseBody
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("hello")
+    @ResponseBody
+    public String helloPost() {
+        return "helloPost";
     }
 }
--- spring-mvc-utilize/src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ spring-mvc-utilize/src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -10,13 +10,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.hasItems;
 
-@RunWith(SpringRunner.class) // Junit 어노테이션, 스프링에서 제공하는 Junit Runner. 스프링 테스트 실행할 때 효율적으로 실행할 수 있도록 도와줌. 내부적으로 애플리케이션 컨텍스트도 만들어줌. 테스트에서 사용할.
-@WebMvcTest //web만 테스트 mock 을 사용할 수 있게 해줌.
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class SampleControllerTest extends TestCase {
 
     @Autowired
@@ -24,13 +26,20 @@ public class SampleControllerTest extends TestCase {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello")
-                        .header(HttpHeaders.FROM,"localhost")
-                //                .header(HttpHeaders.AUTHORIZATION,"111")
-                                .param("name","spring")
+        mockMvc.perform(options("/hello")
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(header().exists(HttpHeaders.ALLOW)) //헤더가 있는지 확인
+        //        .andExpect(header().stringValues(HttpHeaders.ALLOW,"GET,HEAD,POST,OPTIONS")) //순서가 다르면 error 남.
+                //순서 상관없이 찾고 싶다면.
+                .andExpect(header().stringValues(HttpHeaders.ALLOW,
+                        hasItems(containsString("GET"),
+                                containsString("POST"),
+                                containsString("HEAD"),
+                                containsString("OPTIONS")
+                        )
+                ))
         ;
     }
 
